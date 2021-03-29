2dcae4e7998b600bb97f04bc963ef296f1eb48c2
jacobjpelletier <jacobjpelletier@gmail.com>
2021-03-28 20:35:47 -0400

    skeleton discussed 3/28, initialized logic branch

---
+++ seasnake.c
@@ -0,0 +1,43 @@
+/*
+ *  TODO:
+ *
+ *
+ *
+ *
+ */
+
+// example method(){
+   /** method explanation
+    *   expected input
+    *   expected output
+    */
+    // single line comments afterwords
+//}
+
+
+/*
+ *  DRIVER CODE - main()
+ */
+    // initiate..
+    // snakepit()
+    // snake()
+    // trophies()
+    // while(true)
+    //     logic(snake, trophies, snakepit)
+
+/*
+ *  SNAKE PIT -
+ */
+
+/*
+ *  TROPHIES -
+ */
+
+/*
+ *  SNAKE -
+ */
+
+/*
+ *  LOGIC - jacob
+ *  // branch of upstream main called "logic"
+ */
